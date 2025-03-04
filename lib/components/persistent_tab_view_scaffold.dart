864b316c647e67445b8960e09dfce6e09350df1c
furkandivarci <furkan.divarci@karaca.com>
2025-03-04 14:47:10 +0300

    _hideNavBarAnimationController.duration = 300

--- lib/components/persistent_tab_view_scaffold.dart
+++ lib/components/persistent_tab_view_scaffold.dart
@@ -76,7 +76,7 @@ class _PersistentTabViewScaffoldState extends State<PersistentTabViewScaffold>
   late final AnimationController _hideNavBarAnimationController =
       AnimationController(
     vsync: this,
-    duration: Duration.zero,
+    duration: const Duration(milliseconds: 300),
   );
   late final Animation<double> _animation = Tween<double>(
     begin: 1,
